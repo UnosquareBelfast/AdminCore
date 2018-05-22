fca22bd996a05cbeac78878c1cd56ed75be97f80
William-Unosquare <william.park@unosquare.com>
2018-05-22 16:08:21 +0100

    Holiday service converted to use Axios

--- web-app/src/components/BookedLeave/container.js
+++ web-app/src/components/BookedLeave/container.js
@@ -2,31 +2,36 @@ import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import Swal from 'sweetalert2';
 import Moment from 'moment';
-import HolidayService from '../../services/holidayService';
+import { getHolidays } from '../../services/holidayService';
 
-export default (Wrapped) => (
+export default Wrapped =>
   class extends React.Component {
     propTypes = {
       user: PT.object,
-    }
+    };
 
     constructor(props) {
       super(props);
       this.state = {
         bookedHolidays: [],
       };
-      this.HolidayService = new HolidayService();
     }
 
     componentDidMount() {
-      this.HolidayService.getHolidays(this.props.user.id)
-        .then(response =>{
-          const bookedHolidays = response.filter(hol => { return this.isDateInTheFuture(hol.date); });
-          this.setState({bookedLeave: bookedHolidays});
+      getHolidays(this.props.user.id)
+        .then(response => {
+          const bookedHolidays = response.data.filter(hol => {
+            return this.isDateInTheFuture(hol.date);
+          });
+          this.setState({ bookedLeave: bookedHolidays });
         })
-        .catch(error =>{
-            Swal({title: 'Could not get booked holidays', text: error.message, type: 'error'});
-        })
+        .catch(error => {
+          Swal({
+            title: 'Could not get booked holidays',
+            text: error.message,
+            type: 'error',
+          });
+        });
     }
 
     isDateInTheFuture(date) {
@@ -38,5 +43,4 @@ export default (Wrapped) => (
         <Wrapped bookedHolidays={this.state.bookedHolidays} {...this.props} />
       );
     }
-  }
-);
+  };
