420a5b97d9083a4a8647785653e9042d60bba1bf
Fraser Cassidy <fraser.cassidy@unosquare.com>
2018-05-30 12:36:31 +0100

    add border-radius

--- web-app/src/components/styled.js
+++ web-app/src/components/styled.js
@@ -7,11 +7,11 @@ export const Card = styled.div`
   position: relative;
 	display: flex;
 	flex-direction: column;
-	min-width: 0;
+	min-width: 0; 
 	word-wrap: break-word;
 	background-color: #fff;
 	background-clip: border-box;
 	border: 1px solid rgba(0,0,0,.125);
-	border-radius: .25rem;
+  border-radius: .25rem;
 `;
 
