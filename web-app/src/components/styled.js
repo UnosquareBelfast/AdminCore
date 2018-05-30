b8cd948e459948e72aa20926cabfe2a174e34362
fraser-uno <39162509+fraser-uno@users.noreply.github.com>
2018-05-30 12:42:51 +0100

    Update styled.js
    correct indentation

--- web-app/src/components/styled.js
+++ web-app/src/components/styled.js
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 
 export const Card = styled.div`
   display: inline-block;
-	padding: 5px;
-	margin: 8px;	
+  padding: 5px; 
+  margin: 8px;	
   position: relative;
-	display: flex;
-	flex-direction: column;
-	min-width: 0; 
-	word-wrap: break-word;
-	background-color: #fff;
-	background-clip: border-box;
-	border: 1px solid rgba(0,0,0,.125);
+  display: flex;
+  flex-direction: column;
+  min-width: 0; 
+  word-wrap: break-word;
+  background-color: #fff;
+  background-clip: border-box;
+  border: 1px solid rgba(0,0,0,.125);
   border-radius: .25rem;
 `;
 
