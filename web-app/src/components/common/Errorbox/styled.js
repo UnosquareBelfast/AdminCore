3bcbbb88f58dbebb23e41abecb0679c1649d9a44
William-Unosquare <william.park@unosquare.com>
2018-06-01 16:05:19 +0100

    Update Errorbox component to only show if there is an error

--- web-app/src/components/common/Errorbox/styled.js
+++ web-app/src/components/common/Errorbox/styled.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 
 export const ErrorContainer = styled.div`
-  padding: 15px;
+  margin: 1rem 0;
+  padding: 0.5rem;
   background: #ff1a1a;
   border-radius: 4px;
   width: 100%;
   box-sizing: border-box;
   color: white;
+  p {
+    padding: 0;
+    margin: 0;
+  }
+  p:first-of-type {
+    margin-bottom: 0.3rem;
+  }
 `;
