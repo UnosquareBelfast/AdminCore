f42bb25c3b7ddc46778a9adcccef41a866262b61
William-Unosquare <william.park@unosquare.com>
2018-06-12 17:02:22 +0100

    UserDetails card: changed style, added prop-types, role now displays

--- web-app/src/components/UserDetails/index.jsx
+++ web-app/src/components/UserDetails/index.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import container from './container';
-
 import { Card } from '../common';
+import { StyleContainer } from './styled';
+import employeeRoles from '../../utilities/roles';
 
-export const UserDetails = (props) => {
+const roleIdToText = role => {
+  switch (role) {
+    case employeeRoles.ADMIN:
+      return 'Administrator';
+    case employeeRoles.SUPER:
+      return 'Team Leader';
+    case employeeRoles.STANDARD:
+      return 'Employee';
+    default:
+      return '';
+  }
+};
 
-
-const { user: { forename, surname, employeeRoleDescription } } = props;
+export const UserDetails = props => {
+  const {
+    user: { forename, surname, employeeRoleId },
+  } = props;
 
   return (
     <Card>
-      <strong>User Details</strong>
-      <div><span>Name: { `${surname}, ${forename}` }</span></div>
-      <div><span>Role: { employeeRoleDescription } </span></div>
+      <StyleContainer>
+        <strong>Logged in as</strong>
+        <p>Name: {`${surname}, ${forename}`}</p>
+        <p>Role: {roleIdToText(employeeRoleId)} </p>
+      </StyleContainer>
     </Card>
   );
 };
 
+UserDetails.propTypes = {
+  user: PT.object,
+};
 
 export default container(UserDetails);
---
+++ web-app/src/components/UserDetails/styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyleContainer = styled.div`
+  strong {
+    display: block;
+    margin-bottom: 10px;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+`;
