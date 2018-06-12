958352ee661674f6171eb84ea3ae2ac1438823aa
William-Unosquare <39301552+William-Unosquare@users.noreply.github.com>
2018-06-12 17:10:07 +0100

    Update styled.js

--- web-app/src/components/common/Errorbox/styled.js
+++ web-app/src/components/common/Errorbox/styled.js
@@ -1,19 +1,14 @@
 import styled from 'styled-components';
 
 export const ErrorContainer = styled.div`
-<<<<<<< HEAD
-  padding: 15px;
-=======
   margin: 1rem 0;
   padding: 0.5rem;
->>>>>>> c2ee99cd1a1db4e4574d79d63ad4ca47bff4f2dc
   background: #ff1a1a;
   border-radius: 4px;
   width: 100%;
   box-sizing: border-box;
   color: white;
-<<<<<<< HEAD
-=======
+
   p {
     padding: 0;
     margin: 0;
@@ -21,5 +16,4 @@ export const ErrorContainer = styled.div`
   p:first-of-type {
     margin-bottom: 0.3rem;
   }
->>>>>>> c2ee99cd1a1db4e4574d79d63ad4ca47bff4f2dc
 `;
