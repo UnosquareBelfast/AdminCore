72cdb123d2fc59d3768873224cb91dcc09067019
William-Unosquare <william.park@unosquare.com>
2018-06-13 15:39:16 +0100

    Slightly improve modal by adding some width

--- web-app/src/components/common/Modal/styled.js
+++ web-app/src/components/common/Modal/styled.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0,0,0,0.25);
+  background: rgba(0, 0, 0, 0.25);
   z-index: 998;
   display: flex;
   justify-content: center;
@@ -18,8 +18,11 @@ export const Content = styled.div`
   padding: 15px;
   border-radius: 4px;
   margin-top: -10%;
-  box-shadow: 0px 2px 2px rgba(0,0,0,0.1);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
   position: relative;
+  min-width: 300px;
+  max-width: 600px;
+  width: 50%;
 
   span {
     position: absolute;
