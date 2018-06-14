0c3d3e76b3bdb5c20a6fe7e09946da5ce716b477
William-Unosquare <39301552+William-Unosquare@users.noreply.github.com>
2018-06-14 17:23:19 +0100

    Update styled.js

--- web-app/src/components/common/Errorbox/styled.js
+++ web-app/src/components/common/Errorbox/styled.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const ErrorContainer = styled.div`
   margin: 1rem 0;
   padding: 0.5rem;
->>>>>>> c2ee99cd1a1db4e4574d79d63ad4ca47bff4f2dc
   background: #ff1a1a;
   border-radius: 4px;
   width: 100%;
