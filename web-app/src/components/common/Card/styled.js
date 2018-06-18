cacd5cd1c5af7690792bd9170ea76e88a9b1bc99
William-Unosquare <william.park@unosquare.com>
2018-06-18 14:40:50 +0100

    Add class to card component and remove redundent styling

--- web-app/src/components/common/Card/styled.js
+++ web-app/src/components/common/Card/styled.js
@@ -1,18 +1,6 @@
 import styled from 'styled-components';
 
 export const CardContainer = styled.div`
-  ${'' /* display: inline-block;
-  padding: 5px;
-  margin: 8px;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0,0,0,.125);
-  border-radius: .25rem; */}
   display: inline-block;
   padding: 15px;
   background: white;
