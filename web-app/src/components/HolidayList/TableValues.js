9bec0769953b17f728de49d135085dbfe50485df
William-Unosquare <william.park@unosquare.com>
2018-06-27 12:49:48 +0100

    Add new lines  to bottom of files

--- web-app/src/components/HolidayList/TableValues.js
+++ web-app/src/components/HolidayList/TableValues.js
@@ -7,9 +7,9 @@ const status = {
   Header: 'Status',
   sortable: false,
   filterable: false,
-  'accessor': holiday => (
+  accessor: holiday => (
     <Fragment>
-      <StatusDot status={holiday.holidayStatusId}/> 
+      <StatusDot status={holiday.holidayStatusId} />
       {statusText[holiday.holidayStatusId]}
     </Fragment>
   ),
@@ -18,13 +18,19 @@ const status = {
 const employee = {
   id: 'employee',
   Header: 'Employee',
-  'accessor': ({employee}) => `${employee.forename} ${employee.surname}`,
+  accessor: ({ employee }) => `${employee.forename} ${employee.surname}`,
   sortMethod: (a, b) => {
     if (a === b) {
       return 0;
     }
-    const aReverse = a.split('').reverse().join('');
-    const bReverse = b.split('').reverse().join('');
+    const aReverse = a
+      .split('')
+      .reverse()
+      .join('');
+    const bReverse = b
+      .split('')
+      .reverse()
+      .join('');
     return aReverse > bReverse ? 1 : -1;
   },
 };
@@ -32,21 +38,25 @@ const employee = {
 const startDate = {
   id: 'startDate',
   Header: 'Start Date',
-  'accessor': holiday => holiday.start.format('D MMM YYYY'),
+  accessor: holiday => holiday.start.format('D MMM YYYY'),
 };
 
 const endDate = {
   id: 'endDate',
   Header: 'End Date',
-  'accessor': holiday => holiday.end.format('D MMM YYYY'),
+  accessor: holiday => holiday.end.format('D MMM YYYY'),
 };
 
 const requestedDate = {
   id: 'requestedDate',
   Header: 'Requested Date',
-  'accessor': holiday => holiday.requested.format('D MMM YYYY'),
+  accessor: holiday => holiday.requested.format('D MMM YYYY'),
 };
 
 export default {
-  status, startDate, endDate, requestedDate, employee,
+  status,
+  startDate,
+  endDate,
+  requestedDate,
+  employee,
 };
