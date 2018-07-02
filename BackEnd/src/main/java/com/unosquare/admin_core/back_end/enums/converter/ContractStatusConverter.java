510b21be9efbf3c61a2293d3d5ad62cbcd3712f6
Peter Drysdale <peter.drysdale@unosquare.com>
2018-07-02 14:12:42 +0100

    Hot fix for docker build issue

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/enums/converter/ContractStatusConverter.java
+++ BackEnd/src/main/java/com/unosquare/admin_core/back_end/enums/converter/ContractStatusConverter.java
@@ -1,27 +1,27 @@
-package com.unosquare.admin_core.back_end.enums.converter;
-
-import com.unosquare.admin_core.back_end.enums.ContractStatus;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-
-@Converter
-public class ContractStatusConverter
-        implements AttributeConverter<ContractStatus, Integer> {
-
-    public Integer convertToDatabaseColumn(ContractStatus status) {
-        if (status == null) {
-            return null;
-        }
-
-        return status.getContractStatusId();
-    }
-
-    public ContractStatus convertToEntityAttribute(Integer id) {
-        if (id == null) {
-            return null;
-        }
-
-        return ContractStatus.fromId(id);
-    }
-}
+//package com.unosquare.admin_core.back_end.enums.converter;
+//
+//import com.unosquare.admin_core.back_end.enums.ContractStatus;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//
+//@Converter
+//public class ContractStatusConverter
+//        implements AttributeConverter<ContractStatus, Integer> {
+//
+//    public Integer convertToDatabaseColumn(ContractStatus status) {
+//        if (status == null) {
+//            return null;
+//        }
+//
+//        return status.getContractStatusId();
+//    }
+//
+//    public ContractStatus convertToEntityAttribute(Integer id) {
+//        if (id == null) {
+//            return null;
+//        }
+//
+//        return ContractStatus.fromId(id);
+//    }
+//}
