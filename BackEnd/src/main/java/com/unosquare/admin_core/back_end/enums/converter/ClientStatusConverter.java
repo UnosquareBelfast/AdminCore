3e32026372d10ec25271834364d04a7ab829debf
Peter Drysdale <peter.drysdale@unosquare.com>
2018-07-02 14:14:48 +0100

    Undoing changes specific to branch task

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/enums/converter/ClientStatusConverter.java
+++ BackEnd/src/main/java/com/unosquare/admin_core/back_end/enums/converter/ClientStatusConverter.java
@@ -1,28 +1,28 @@
-//package com.unosquare.admin_core.back_end.enums.converter;
-//
-//import com.unosquare.admin_core.back_end.enums.ClientStatus;
-//
-//import javax.persistence.AttributeConverter;
-//import javax.persistence.Converter;
-//
-//@Converter
-//public class ClientStatusConverter
-//        implements AttributeConverter<ClientStatus, Integer> {
-//
-//    public Integer convertToDatabaseColumn(ClientStatus status) {
-//        if (status == null) {
-//            return null;
-//        }
-//
-//        return status.getClientStatusId();
-//    }
-//
-//    @Override
-//    public ClientStatus convertToEntityAttribute(Integer id) {
-//        if (id == null) {
-//            return null;
-//        }
-//
-//        return ClientStatus.fromId(id);
-//    }
-//}
+package com.unosquare.admin_core.back_end.enums.converter;
+
+import com.unosquare.admin_core.back_end.enums.ClientStatus;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class ClientStatusConverter
+        implements AttributeConverter<ClientStatus, Integer> {
+
+    public Integer convertToDatabaseColumn(ClientStatus status) {
+        if (status == null) {
+            return null;
+        }
+
+        return status.getClientStatusId();
+    }
+
+    @Override
+    public ClientStatus convertToEntityAttribute(Integer id) {
+        if (id == null) {
+            return null;
+        }
+
+        return ClientStatus.fromId(id);
+    }
+}
