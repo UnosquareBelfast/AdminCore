8a8e1c40132abc3ee6b9d27a2d4aeb67d4a50252
Paul Matchett <p.matchett80@gmail.com>
2018-07-03 11:52:07 +0100

    added input component to login

--- web-app/src/pages/Login/container.js
+++ web-app/src/pages/Login/container.js
@@ -4,25 +4,96 @@ import { PropTypes as PT } from 'prop-types';
 import { userLogin } from '../../services/userService';
 import { isLoggedIn } from '../../utilities/currentUser';
 
+import {
+  getFormElementsArray,
+  updateFormDataOnChange,
+  isFormValidOnChange,
+  getFormDataOnSubmit,
+} from '../../utilities/forms';
+
 export default Wrapped =>
   class extends React.Component {
     static propTypes = {
       history: PT.object,
     };
+    constructor(props) {
+      super(props);
+      this.state = {
+        form: this.initialFormState(),
+        formIsValid: false,
+        success: null,
+        error: null,
+        loading: false,
+      };
+    }
 
     componentWillMount() {
       if (isLoggedIn()) this.props.history.replace('/');
     }
 
-    handleChange = e => {
-      this.setState({
-        [e.target.name]: e.target.value,
-      });
+    initialFormState() {
+      return {
+        email: {
+          label: 'Email:',
+          elementType: 'input',
+          elementConfig: {
+            type: 'email',
+            placeholder: 'Enter your email address',
+          },
+          value: '',
+          validation: {
+            required: true,
+            isEmail: true,
+          },
+          valid: false,
+          touched: false,
+        },
+        password: {
+          label: 'Password:',
+          elementType: 'input',
+          elementConfig: {
+            type: 'password',
+            placeholder: 'Enter your password',
+          },
+          value: '',
+          validation: {
+            required: true,
+            minLength: 4,
+          },
+          valid: false,
+          touched: false,
+        },
+        // remember: {
+        //   label: 'Remember Credentials:',
+        //   elementType: 'checkbox',
+        //   elementConfig: {
+        //     type: 'checkbox',
+        //     placeholder: 'Remember Credentials',
+        //   },
+        //   value: true,
+        //   validation: {},
+        //   valid: true,
+        // },
+      };
+    }
+
+    handleFormChange = (event, inputIdentifier) => {
+      const inputValue = event.target.value;
+      const updatedForm = updateFormDataOnChange(
+        this.state.form,
+        inputIdentifier,
+        inputValue,
+      );
+
+      let formIsValid = isFormValidOnChange(updatedForm);
+      this.setState({ form: updatedForm, formIsValid: formIsValid });
     };
 
-    handleSubmit = e => {
+    handleFormSubmit = e => {
       e.preventDefault();
-      userLogin(this.state.email, this.state.password)
+      const formData = getFormDataOnSubmit(this.state.form);
+
+      userLogin(formData.email, formData.password)
         .then(() => {
           this.props.history.push('/');
         })
@@ -38,8 +109,13 @@ export default Wrapped =>
     render() {
       return (
         <Wrapped
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
+          formElementsArray={getFormElementsArray(this.state.form)}
+          formIsValid={this.state.formIsValid}
+          submitForm={this.handleFormSubmit}
+          formChanged={(event, id) => this.handleFormChange(event, id)}
+          success={this.state.success}
+          error={this.state.error}
+          loading={this.state.loading}
         />
       );
     }
---
+++ web-app/src/pages/Login/styled.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const LoginBG = styled.div`
+  background-color: ${props => props.theme.colours.unoBlue};
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginPanel = styled.div`
+  background-color: #fff;
+  border-radius: 15px;
+  padding: 40px;
+
+  h1 {
+    margin: 0 0 20px 0;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background-color: ${props => props.theme.colours.lightgrey};
+    padding: 30px 20px;
+    box-sizing: border-box;
+    width: 420px;
+  }
+`;
