e63a1f377bcd3847153c6d002580f2762c45ddd2
kirsty-unosquare <39771206+kirsty-unosquare@users.noreply.github.com>
2018-07-10 16:30:19 +0100

    Feat/datepicker config (ios) (#75)
    * Configure ios datepicker. Fix end date not updating bug.
    
    * remove extra View
    
    * Add start and end date title
    
    * Set default for minimumDate prop.

--- NativeApp/src/components/Booking/View/index.js
+++ NativeApp/src/components/Booking/View/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {
   View,
-  Text,
   Button,
+  Text,
   StyleSheet,
 } from 'react-native';
 import { PropTypes as PT } from 'prop-types';
-import moment from 'moment';
 import { CustomDatePickerIOS } from '../../Common';
 
 const BookingView = (props) => {
@@ -18,26 +17,23 @@ const BookingView = (props) => {
     submitRequest,
   } = props;
 
-  const formatDate = date => moment(date).toDate();
 
   return (
     <View style={styles.container}>
       <Text>
-        Starting
-        {'\n'}
-        {startDate}
+        Start date
       </Text>
       <CustomDatePickerIOS
-        chosenDate={formatDate(startDate)}
+        chosenDate={startDate}
         setDate={changeStartDate}
       />
       <Text>
-        Ending
+        End date
       </Text>
       <CustomDatePickerIOS
-        chosenDate={formatDate(endDate)}
+        chosenDate={endDate}
         setDate={changeEndDate}
-        minimumDate={formatDate(startDate)}
+        minimumDate={startDate}
       />
       <Button onPress={submitRequest} title="Request Holiday" />
     </View>
@@ -56,6 +52,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-start',
+    alignItems: 'center',
     paddingTop: 50,
     backgroundColor: '#fff',
   },
--- NativeApp/src/components/Common/CustomDatePickerIOS/index.js
+++ NativeApp/src/components/Common/CustomDatePickerIOS/index.js
@@ -1,29 +1,122 @@
-import React from 'react';
-import { View, DatePickerIOS } from 'react-native';
+import React, { Component, Fragment } from 'react';
+import {
+  View,
+  Text,
+  DatePickerIOS,
+  Modal,
+  StyleSheet,
+  Button,
+  TouchableHighlight,
+} from 'react-native';
 import { PropTypes as PT } from 'prop-types';
+import moment from 'moment';
 
-const CustomDatePickerIOS = (props) => {
-  const { chosenDate, setDate, minimumDate } = props;
-  return (
-    <View>
-      <DatePickerIOS
-        date={chosenDate}
-        onDateChange={setDate}
-        mode="date"
-        minimumDate={minimumDate}
-      />
-    </View>
-  );
-};
+class CustomDatePickerIOS extends Component {
+  static propTypes = {
+    chosenDate: PT.string.isRequired,
+    setDate: PT.func.isRequired,
+    minimumDate: PT.string,
+  };
 
-CustomDatePickerIOS.defaultProps = {
-  minimumDate: new Date(),
-};
+  static defaultProps = {
+    minimumDate: moment().format('YYYY-MM-DD'),
+  };
 
-CustomDatePickerIOS.propTypes = {
-  chosenDate: PT.instanceOf(Date).isRequired,
-  setDate: PT.func.isRequired,
-  minimumDate: PT.instanceOf(Date),
-};
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisable: false,
+    };
+  }
+
+  setModalVisible(visible) {
+    this.setState({ modalVisable: visible });
+  }
+
+  render() {
+    const {
+      chosenDate,
+      setDate,
+      minimumDate,
+    } = this.props;
+
+    const { modalVisable } = this.state;
+
+    const formatDate = date => moment(date).toDate();
+
+    return (
+      <Fragment>
+        {modalVisable && <View style={styles.overlay} />}
+        <TouchableHighlight
+          underlayColor="transparent"
+          onPress={() => this.setModalVisible(true)}
+          style={styles.container}
+        >
+          <View>
+            <View style={styles.dateInput}>
+              <Text>
+                {chosenDate}
+              </Text>
+            </View>
+
+            <Modal
+              animationType="slide"
+              transparent
+              visible={modalVisable}
+            >
+              <View style={styles.dateContainer}>
+                <View style={styles.background}>
+                  <DatePickerIOS
+                    date={formatDate(chosenDate)}
+                    onDateChange={setDate}
+                    mode="date"
+                    minimumDate={formatDate(minimumDate)}
+                  />
+                  <View style={styles.buttonContainer}>
+                    <Button onPress={() => this.setModalVisible(false)} title="Done" />
+                  </View>
+                </View>
+              </View>
+            </Modal>
+
+          </View>
+        </TouchableHighlight>
+      </Fragment>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+  },
+  dateInput: {
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderColor: '#aaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  background: {
+    backgroundColor: 'white',
+  },
+  buttonContainer: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    paddingBottom: 30,
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+  },
+});
 
 export default CustomDatePickerIOS;
