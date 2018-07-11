8489e19b9803ca9d3b12310027d4c926bd1e5fb9
Paul Matchett <p.matchett80@gmail.com>
2018-07-11 11:53:33 +0100

    create user updated with form component

--- web-app/src/components/CreateUser/container.js
+++ web-app/src/components/CreateUser/container.js
@@ -3,19 +3,21 @@ import moment from 'moment';
 import employeeStatus from '../../utilities/employeeStatus';
 import { createUser } from '../../services/userService';
 
-import {
-  getFormElementsArray,
-  updateFormDataOnChange,
-  isFormValidOnChange,
-  getFormDataOnSubmit,
-} from '../../utilities/forms';
-
 export default Wrapped =>
   class extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        form: this.initialFormState(),
+        formData: {
+          forename: '',
+          surname: '',
+          email: '',
+          password: '',
+          country: 1,
+          status: employeeStatus.ACTIVE,
+          employeeRole: 3,
+          startDate: moment(),
+        },
         formIsValid: false,
         success: null,
         error: null,
@@ -23,139 +25,18 @@ export default Wrapped =>
       };
     }
 
-    initialFormState() {
-      return {
-        forename: {
-          label: 'Forename:',
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Enter your forename',
-          },
-          value: '',
-          validation: {
-            required: true,
-          },
-          valid: false,
-          touched: false,
-        },
-        surname: {
-          label: 'Surname:',
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Enter your surname',
-          },
-          value: '',
-          validation: {
-            required: true,
-          },
-          valid: false,
-          touched: false,
-        },
-        email: {
-          label: 'Email:',
-          elementType: 'input',
-          elementConfig: {
-            type: 'email',
-            placeholder: 'Enter your email address',
-          },
-          value: '',
-          validation: {
-            required: true,
-            isEmail: true,
-          },
-          valid: false,
-          touched: false,
-        },
-        password: {
-          label: 'Password:',
-          elementType: 'input',
-          elementConfig: {
-            type: 'password',
-            placeholder: 'Enter a password',
-          },
-          value: '',
-          validation: {
-            required: true,
-            minLength: 6,
-          },
-          valid: false,
-          touched: false,
-        },
-        country: {
-          label: 'Country:',
-          elementType: 'select',
-          elementConfig: {
-            options: [
-              { value: 1, displayValue: 'Northern Ireland' },
-              { value: 2, displayValue: 'Mexico' },
-            ],
-          },
-          value: 1,
-          validation: {},
-          valid: true,
-        },
-        status: {
-          //TODO Create enum file employeeStatus.ACTIVE
-          label: 'Status:',
-          elementType: 'select',
-          elementConfig: {
-            options: [
-              { value: employeeStatus.ACTIVE, displayValue: 'Active' },
-              { value: employeeStatus.INACTIVE, displayValue: 'Inactive' },
-            ],
-          },
-          value: 1,
-          validation: {},
-          valid: true,
-        },
-        employeeRole: {
-          label: 'Employee Role:',
-          elementType: 'select',
-          elementConfig: {
-            options: [
-              { value: 1, displayValue: 'Team Leader' },
-              { value: 2, displayValue: 'System Admin' },
-              { value: 3, displayValue: 'Employee' },
-            ],
-          },
-          value: 3,
-          validation: {},
-          valid: true,
-        },
-        startDate: {
-          label: 'Start Date:',
-          elementType: 'date',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Enter a start date',
-          },
-          value: moment(),
-          validation: {},
-          valid: true,
-        },
-      };
+    handleFormStatus(name, value, formIsValid) {
+      const updatedFormData = { ...this.state.formData };
+      updatedFormData[name] = value;
+      this.setState({
+        formData: updatedFormData,
+        formIsValid,
+      });
     }
 
-    handleFormChange = (event, inputIdentifier) => {
-      const inputValue =
-        inputIdentifier !== 'startDate' ? event.target.value : event;
-
-      const updatedForm = updateFormDataOnChange(
-        this.state.form,
-        inputIdentifier,
-        inputValue,
-      );
-
-      let formIsValid = isFormValidOnChange(updatedForm);
-      this.setState({ form: updatedForm, formIsValid: formIsValid });
-    };
-
-    handleFormSubmit = e => {
-      e.preventDefault();
-      const formData = getFormDataOnSubmit(this.state.form);
-
+    handleFormSubmit = event => {
+      event.preventDefault();
+      const formData = { ...this.state.formData };
       formData.startDate = formData.startDate.toISOString();
       this.setState({ loading: true });
       return createUser(formData)
@@ -163,7 +44,6 @@ export default Wrapped =>
           this.setState({
             loading: false,
             success: true,
-            form: this.initialFormState(),
           });
         })
         .catch(error => {
@@ -174,13 +54,14 @@ export default Wrapped =>
     render() {
       return (
         <Wrapped
-          formElementsArray={getFormElementsArray(this.state.form)}
-          formIsValid={this.state.formIsValid}
-          submitForm={this.handleFormSubmit}
-          formChanged={(event, id) => this.handleFormChange(event, id)}
           success={this.state.success}
           error={this.state.error}
           loading={this.state.loading}
+          formData={this.state.formData}
+          formStatus={(name, value, formIsValid) =>
+            this.handleFormStatus(name, value, formIsValid)
+          }
+          submitForm={e => this.handleFormSubmit(e)}
         />
       );
     }
--- web-app/src/components/CreateUser/index.jsx
+++ web-app/src/components/CreateUser/index.jsx
@@ -1,56 +1,144 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import container from './container';
-import { Card, Input, Button, Errorbox } from '../common';
-import { Form, ButtonWrap } from './styled';
+import employeeStatus from '../../utilities/employeeStatus';
+import { Card, Form, Input, Errorbox } from '../common';
+import { FormContainer } from './styled';
 
 export const CreateUser = props => {
-  const {
-    formElementsArray,
-    formIsValid,
-    submitForm,
-    formChanged,
-    error,
-    success,
-    loading,
-  } = props;
+  const { error, success, loading, submitForm, formStatus, formData } = props;
 
   let form;
   if (loading) {
     form = null;
   } else {
     form = (
-      <Form autoComplete="off">
-        {formElementsArray.map(({ id, config }, index) => (
-          <Input
-            key={id}
-            label={config.label}
-            elementType={config.elementType}
-            elementConfig={config.elementConfig}
-            value={config.value}
-            invalid={!config.valid}
-            shouldValidate={config.validation}
-            focus={index === 0 ? true : false}
-            touched={config.touched}
-            changed={event => formChanged(event, id)}
-          />
-        ))}
-        <ButtonWrap>
-          <Button
-            id="createUserBtn"
-            label="Create User"
-            onClick={submitForm}
-            disabled={!formIsValid}
-          />
-        </ButtonWrap>
+      <Form
+        formData={formData}
+        submitForm={submitForm}
+        formStatus={formStatus}
+        buttonLabel="Create user"
+      >
+        <Input
+          type="input"
+          htmlAttrs={{
+            type: 'input',
+            name: 'forename',
+            placeholder: 'Enter a forename',
+          }}
+          value={formData.forename}
+          focus
+          label="Forename:"
+          rules={{
+            required: true,
+          }}
+        />
+        <Input
+          type="input"
+          htmlAttrs={{
+            type: 'input',
+            name: 'surname',
+            placeholder: 'Enter a surname',
+          }}
+          value={formData.surname}
+          focus={false}
+          label="Surname:"
+          rules={{
+            required: true,
+          }}
+        />
+        <Input
+          type="input"
+          htmlAttrs={{
+            type: 'email',
+            name: 'email',
+            placeholder: 'Enter an email',
+          }}
+          value={formData.email}
+          focus={false}
+          label="Email:"
+          rules={{
+            required: true,
+            isEmail: true,
+          }}
+        />
+        <Input
+          type="input"
+          htmlAttrs={{
+            type: 'password',
+            name: 'password',
+            placeholder: 'Enter an password',
+          }}
+          value={formData.password}
+          focus={false}
+          label="Password:"
+          rules={{
+            required: true,
+            minLength: 6,
+          }}
+        />
+        <Input
+          type="select"
+          htmlAttrs={{
+            name: 'country',
+            options: [
+              { value: 1, displayValue: 'Northern Ireland' },
+              { value: 2, displayValue: 'Mexico' },
+            ],
+          }}
+          value={formData.country}
+          focus={false}
+          label="Country:"
+        />
+        <Input
+          type="select"
+          htmlAttrs={{
+            name: 'status',
+            options: [
+              { value: employeeStatus.ACTIVE, displayValue: 'Active' },
+              { value: employeeStatus.INACTIVE, displayValue: 'Inactive' },
+            ],
+          }}
+          value={formData.status}
+          focus={false}
+          label="Status:"
+        />
+        <Input
+          type="select"
+          htmlAttrs={{
+            name: 'employeeRole',
+            options: [
+              { value: 1, displayValue: 'Team Leader' },
+              { value: 2, displayValue: 'System Admin' },
+              { value: 3, displayValue: 'Employee' },
+            ],
+          }}
+          value={formData.employeeRole}
+          focus={false}
+          label="Employee Role:"
+        />
+        <Input
+          type="date"
+          htmlAttrs={{
+            type: 'input',
+            name: 'startDate',
+            placeholder: 'Enter a start date',
+          }}
+          value={formData.startDate}
+          rules={{
+            dateNotInPast: true,
+          }}
+          focus={false}
+          label="Start Date:"
+        />
       </Form>
     );
   }
 
   return (
     <Card>
-      <h3>Create Employee</h3>
-      {form}
+      <h3>Create User</h3>
+      <FormContainer>{form}</FormContainer>
       <Errorbox
         id="errorCreateUser"
         error={error}
@@ -62,13 +150,12 @@ export const CreateUser = props => {
 };
 
 CreateUser.propTypes = {
-  formElementsArray: PT.array.isRequired,
-  formIsValid: PT.bool.isRequired,
-  submitForm: PT.func.isRequired,
-  formChanged: PT.func.isRequired,
   loading: PT.bool.isRequired,
   error: PT.object,
   success: PT.bool,
+  formData: PT.object.isRequired,
+  submitForm: PT.func.isRequired,
+  formStatus: PT.func.isRequired,
 };
 
 CreateUser.defaultProps = {
