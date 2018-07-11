7209d8b7ed5e2eb019a618d4c748c11107281de8
Paul Brownlee-Unosquare <paul.brownlee@unosquare.com>
2018-07-11 17:20:17 +0100

    Updated AppConfig-Removal of unnecessary imports

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/configuration/AppConfig.java
+++ BackEnd/src/main/java/com/unosquare/admin_core/back_end/configuration/AppConfig.java
@@ -1,36 +1,15 @@
 package com.unosquare.admin_core.back_end.configuration;
 import com.unosquare.admin_core.back_end.configuration.mappings.*;
-import com.unosquare.admin_core.back_end.dto.*;
-import com.unosquare.admin_core.back_end.entity.*;
-import com.unosquare.admin_core.back_end.enums.converter.ContractStatusConverter;
-import com.unosquare.admin_core.back_end.payload.SignUpRequest;
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import org.modelmapper.ModelMapper;
-
-import java.io.Console;
-import java.io.File;
-import java.io.FileInputStream;
-import java.lang.reflect.Method;
-import org.modelmapper.PropertyMap;
-import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import sun.security.ssl.Debug;
 import java.lang.*;
 import com.unosquare.admin_core.back_end.configuration.mappings.BaseMappings;
-import java.lang.reflect.Constructor;
-
-import javax.print.attribute.standard.Destination;
-import java.lang.reflect.Type;
-import java.net.URL;
-import java.time.LocalDate;
 import java.util.*;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
 
 @Configuration
 @EnableTransactionManagement
