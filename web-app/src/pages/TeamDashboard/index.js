084a98b6690baf982e27d5b9dc9eb3095d7985fa
William-Unosquare <william.park@unosquare.com>
2018-07-17 12:07:49 +0100

    Build page skeleton

--- web-app/src/pages/TeamDashboard/index.js
+++ web-app/src/pages/TeamDashboard/index.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
-import { Container } from './styled';
+import { Container, Columns, Stat } from './styled';
+import { HolidayList } from '../../components/HolidayList';
 
 export const User = props => {
-  return <Container>hey</Container>;
+  return (
+    <Container>
+      <h2>My Team</h2>
+      <Columns>
+        <Stat>
+          <h1>6 MEMBERS</h1>
+          <h4>Active</h4>
+        </Stat>
+        <Stat>
+          <h1>2 MEMBERS</h1>
+          <h4>On Holiday</h4>
+        </Stat>
+        <Stat>
+          <h1>4 MEMBERS</h1>
+          <h4>Working from home</h4>
+        </Stat>
+      </Columns>
+
+      <Columns>
+        <div>
+          <h3>Active Members</h3>
+        </div>
+        <div>
+          <h3>Pending Holidays</h3>
+          <HolidayList
+            holidays={[]}
+            columns={['employee', 'startDate', 'endDate']}
+          />
+        </div>
+      </Columns>
+    </Container>
+  );
 };
 
 User.propTypes = {
--- web-app/src/pages/TeamDashboard/styled.js
+++ web-app/src/pages/TeamDashboard/styled.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.div``;
+
+export const Stat = styled.div`
+  max-width: 200px;
+  h1 {
+    margin-bottom: 5px;
+    color: ${props => props.theme.colours.unoBlue};
+  }
+`;
+
+export const Columns = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+  div {
+    width: 50%;
+  }
+`;
