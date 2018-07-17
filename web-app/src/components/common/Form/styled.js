db2c9f506f5755ed19b61b108e7c19dc805c2832
William-Unosquare <william.park@unosquare.com>
2018-07-17 10:39:32 +0100

    Style changes to Form cpnt to make more flexible with multiple buttons

--- web-app/src/components/common/Form/styled.js
+++ web-app/src/components/common/Form/styled.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.div`
   margin: 20px 0px 0 0;
   display: flex;
-  flex-direction: column;
-  @media (min-width: 992px) {
-    flex-direction: row;
-  }
+  flex-direction: row;
 
   button {
-    margin: 0 0 10px 0;
+    margin: 0 10px 10px 0;
     @media (min-width: 992px) {
-      margin: 0px 4px;
+      margin: 0px 10px 10px 0;
+    }
+    :last-of-type {
+      margin-right: 0;
     }
   }
 `;
