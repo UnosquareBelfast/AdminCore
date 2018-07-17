459bacb609fc9ef80e432b6ac26830158f78d9a0
Paul Matchett <p.matchett80@gmail.com>
2018-07-17 14:03:55 +0100

    updating route for create and update client

--- web-app/src/pages/Admin/index.jsx
+++ web-app/src/pages/Admin/index.jsx
@@ -30,8 +30,12 @@ export const Admin = props => (
         />
         <Route path="/admin/pendingHolidays" component={PendingHolidays} />
         <Route path="/admin/holidays" component={AllHolidays} />
-        <Route path="/admin/clients" component={AllClients} />
+        <Route
+          path="/admin/clients"
+          render={() => <AllClients history={props.history} />}
+        />
         <Route path="/admin/createClient" component={CreateClient} />
+        <Route path="/admin/updateClient/:clientId" component={CreateClient} />
         <Route path="/admin" component={AdminDashboard} />
       </Switch>
     </MainContentContainer>
