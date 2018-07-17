5c346300650d41720d2147e43f55e8422882726a
William-Unosquare <william.park@unosquare.com>
2018-07-17 12:20:01 +0100

    Add icons

--- web-app/src/pages/TeamDashboard/index.js
+++ web-app/src/pages/TeamDashboard/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import { Container, Columns, Stat } from './styled';
 import { HolidayList } from '../../components/HolidayList';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faChild, faMap, faHome } from '@fortawesome/fontawesome-free-solid';
 
 export const User = props => {
   return (
@@ -9,16 +11,22 @@ export const User = props => {
       <h2>My Team</h2>
       <Columns>
         <Stat>
-          <h1>6 MEMBERS</h1>
-          <h4>Active</h4>
+          <h1>8 MEMBERS</h1>
+          <h4>
+            <FontAwesomeIcon icon={faChild} /> Active
+          </h4>
         </Stat>
         <Stat>
           <h1>2 MEMBERS</h1>
-          <h4>On Holiday</h4>
+          <h4>
+            <FontAwesomeIcon icon={faMap} /> On Holiday
+          </h4>
         </Stat>
         <Stat>
           <h1>4 MEMBERS</h1>
-          <h4>Working from home</h4>
+          <h4>
+            <FontAwesomeIcon icon={faHome} /> Working from home
+          </h4>
         </Stat>
       </Columns>
 
