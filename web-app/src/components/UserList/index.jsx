8b2ac05e29dea580ce70ec96e06035c3d89dd6be
William-Unosquare <william.park@unosquare.com>
2018-07-20 12:29:38 +0100

    Removed old code based around action buttons

--- web-app/src/components/UserList/index.jsx
+++ web-app/src/components/UserList/index.jsx
@@ -3,46 +3,17 @@ import { PropTypes as PT } from 'prop-types';
 import container from './container';
 import ReactTable from 'react-table';
 import TableValues from './TableValues';
-import ActionButton from './ActionButton';
-import { ActionWrap } from './styled';
-import { isEmpty } from 'lodash';
 
-const buildActions = (holiday, actions) => {
-  const actionComponents = [];
-
-  for (const action in actions) {
-    actionComponents.push(
-      <ActionButton
-        key={action}
-        holiday={holiday}
-        action={actions[action]}
-        label={action}
-      />
-    );
-  }
-
-  return <ActionWrap>{actionComponents}</ActionWrap>;
-};
-
-const buildColumns = (columns, actions) => {
+const buildColumns = columns => {
   const formattedColumns = columns.reduce((acc, column) => {
     return acc.concat(TableValues[column]);
   }, []);
 
-  if (isEmpty(actions)) return formattedColumns;
-
-  formattedColumns.push({
-    id: 'actions',
-    sortable: false,
-    filterable: false,
-    accessor: user => buildActions(user, actions),
-  });
-
   return formattedColumns;
 };
 
-const renderTable = (users, columns, actions, onRowClick) => {
-  const formattedColumns = buildColumns(columns, actions);
+const renderTable = (users, columns, onRowClick) => {
+  const formattedColumns = buildColumns(columns);
   return (
     <ReactTable
       data={users}
@@ -66,18 +37,17 @@ const renderTable = (users, columns, actions, onRowClick) => {
 };
 
 export const UserList = props => {
-  const { users, columns, actions, onRowClick } = props;
+  const { users, columns, onRowClick } = props;
   return !users || users.length === 0 ? (
     <p>There are no users to show</p>
   ) : (
-    renderTable(users, columns, actions, onRowClick)
+    renderTable(users, columns, onRowClick)
   );
 };
 
 UserList.propTypes = {
   users: PT.array,
   columns: PT.array.isRequired,
-  actions: PT.object,
   onRowClick: PT.func,
 };
 
