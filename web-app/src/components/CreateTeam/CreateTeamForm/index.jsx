7a6bb7ada6dd510ff62a742ee5444ad089a3437d
William-Unosquare <william.park@unosquare.com>
2018-07-23 16:12:01 +0100

    Add styling to form component

--- web-app/src/components/CreateTeam/CreateTeamForm/index.jsx
+++ web-app/src/components/CreateTeam/CreateTeamForm/index.jsx
@@ -2,47 +2,50 @@ import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import container from './container';
 import { Form, Input } from '../../common';
+import { FormContainer } from './styled';
 
 export const CreateTeamForm = props => {
   const { submitForm, formStatus, formData, formIsValid, clients } = props;
 
   return (
-    <Form
-      formData={formData}
-      formStatus={formStatus}
-      actions={[
-        {
-          label: 'Create Team',
-          event: submitForm,
-          disabled: !formIsValid,
-        },
-      ]}
-    >
-      <Input
-        label="Client:"
-        type="select"
-        htmlAttrs={{
-          name: 'selectedClient',
-          options: clients,
-          disabled: clients.length === 0,
-        }}
-        value={formData.selectedClient}
-      />
-      <Input
-        label="Team Name:"
-        type="input"
-        htmlAttrs={{
-          type: 'input',
-          name: 'teamName',
-          placeholder: 'Enter a team name',
-          disabled: clients.length === 0,
-        }}
-        value={formData.teamName}
-        rules={{
-          required: true,
-        }}
-      />
-    </Form>
+    <FormContainer>
+      <Form
+        formData={formData}
+        formStatus={formStatus}
+        actions={[
+          {
+            label: 'Create Team',
+            event: submitForm,
+            disabled: !formIsValid,
+          },
+        ]}
+      >
+        <Input
+          label="Client:"
+          type="select"
+          htmlAttrs={{
+            name: 'selectedClient',
+            options: clients,
+            disabled: clients.length === 0,
+          }}
+          value={formData.selectedClient}
+        />
+        <Input
+          label="Team Name:"
+          type="input"
+          htmlAttrs={{
+            type: 'input',
+            name: 'teamName',
+            placeholder: 'Enter a team name',
+            disabled: clients.length === 0,
+          }}
+          value={formData.teamName}
+          rules={{
+            required: true,
+          }}
+        />
+      </Form>
+    </FormContainer>
   );
 };
 
