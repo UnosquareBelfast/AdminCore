dbbbc2efd41bc8538efa6c19ef3bef4f18cc325d
William-Unosquare <william.park@unosquare.com>
2018-07-23 14:19:30 +0100

    Add CreateTeamForm component

---
+++ web-app/src/components/CreateTeam/CreateTeamForm/container.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { PropTypes as PT } from 'prop-types';
+import { getAllClients } from '../../../services/clientService';
+import swal from 'sweetalert2';
+
+export default Wrapped =>
+  class extends Component {
+    static propTypes = {
+      onSuccess: PT.func,
+      onFailed: PT.func,
+    };
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        formData: {
+          selectedClient: -1,
+          teamName: '',
+        },
+        formIsValid: false,
+        clients: [],
+      };
+    }
+
+    componentDidMount() {
+      getAllClients()
+        .then(response => {
+          const clients = response.data;
+          const formattedClients = clients.reduce((acc, client) => {
+            acc.push({
+              value: client.clientId,
+              displayValue: client.clientName,
+            });
+            return acc;
+          }, []);
+          this.setState({ clients: formattedClients });
+        })
+        .catch(error =>
+          swal('Error', `Could not retreive clients: ${error.message}`, 'error')
+        );
+    }
+
+    handleFormStatus(name, value, formIsValid) {
+      const updatedFormData = { ...this.state.formData };
+      updatedFormData[name] = value;
+      this.setState({
+        formData: updatedFormData,
+        formIsValid,
+      });
+    }
+
+    handleFormSubmit = event => {
+      event.preventDefault();
+    };
+
+    render() {
+      return (
+        <Wrapped
+          clients={this.state.clients}
+          formData={this.state.formData}
+          formIsValid={this.state.formIsValid}
+          formStatus={(name, value, formIsValid) =>
+            this.handleFormStatus(name, value, formIsValid)
+          }
+          submitForm={e => this.handleFormSubmit(e)}
+        />
+      );
+    }
+  };
---
+++ web-app/src/components/CreateTeam/CreateTeamForm/index.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { PropTypes as PT } from 'prop-types';
+import container from './container';
+import { Form, Input } from '../../common';
+
+export const CreateTeamForm = props => {
+  const { submitForm, formStatus, formData, formIsValid, clients } = props;
+
+  return (
+    <Form
+      formData={formData}
+      formStatus={formStatus}
+      actions={[
+        {
+          label: 'Create Contract',
+          event: submitForm,
+          disabled: !formIsValid,
+        },
+      ]}
+    >
+      <Input
+        label="Client:"
+        type="select"
+        htmlAttrs={{
+          name: 'selectedClient',
+          options: clients,
+        }}
+        value={formData.selectedClient}
+      />
+      <Input
+        label="Team Name:"
+        type="input"
+        htmlAttrs={{
+          type: 'input',
+          name: 'teamName',
+          placeholder: 'Enter a team name',
+        }}
+        value={formData.teamName}
+        rules={{
+          required: true,
+        }}
+      />
+    </Form>
+  );
+};
+
+CreateTeamForm.propTypes = {
+  clients: PT.array,
+  formData: PT.object.isRequired,
+  submitForm: PT.func.isRequired,
+  formStatus: PT.func.isRequired,
+  formIsValid: PT.bool.isRequired,
+};
+
+CreateTeamForm.defaultProps = {
+  clients: [],
+};
+
+export default container(CreateTeamForm);
