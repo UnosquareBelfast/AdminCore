16a73319795558801dbffee2d46b2cc448f0cfb8
William-Unosquare <william.park@unosquare.com>
2018-07-23 14:19:09 +0100

    Add CreateTeamForm to create team page

--- web-app/src/components/CreateTeam/index.jsx
+++ web-app/src/components/CreateTeam/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import container from './container';
+import CreateTeamForm from './CreateTeamForm';
 
 export const CreateTeam = props => {
   return (
     <div>
       <h2>Create Team</h2>
+      <CreateTeamForm />
     </div>
   );
 };
