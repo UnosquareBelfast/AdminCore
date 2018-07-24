e4d3f63aa6edd6f3afc9064cae222d8f50807515
Paul Matchett <p.matchett80@gmail.com>
2018-07-24 10:05:57 +0100

    added styling to calendar header

--- web-app/src/pages/Dashboard/styled.js
+++ web-app/src/pages/Dashboard/styled.js
@@ -7,4 +7,19 @@ export const InnerLayout = styled.div`
   .rbc-calendar {
     height: 700px;
   }
+
+  .rbc-header {
+    padding: 10px 3px;
+    background: #0eb5d1;
+    color: white;
+  }
+
+  .rbc-off-range-bg {
+    background: #e5e5e5;
+    cursor: not-allowed;
+  }
+
+  .rbc-today {
+    background-color: rgba(14, 181, 209, 0.5);
+  }
 `;
