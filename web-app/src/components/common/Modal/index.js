042d2e260a49a99f4968f5a262d88715dd2a8c29
William-Unosquare <william.park@unosquare.com>
2018-07-25 14:12:18 +0100

    Update our modal component to use rodal

--- web-app/src/components/common/Modal/index.js
+++ web-app/src/components/common/Modal/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { PropTypes as PT } from 'prop-types';
-import { Container, Content } from './styled';
+import Rodal from 'rodal';
 
-const Modal = ({children}) => (
-  <Container>
-    <Content>  
-      {children}
-    </Content>
-  </Container>
+const Modal = ({ children, visible, closeModal }) => (
+  <Rodal
+    visible={visible}
+    onClose={closeModal}
+    customStyles={{
+      width: '50%',
+      minWidth: '250px',
+      height: 'auto',
+      bottom: 'auto',
+      top: '50%',
+      transform: 'translateY(-50%)',
+    }}
+  >
+    {children}
+  </Rodal>
 );
 
 Modal.propTypes = {
-  children: PT.node,
+  children: PT.node.isRequired,
+  closeModal: PT.func,
+  visible: PT.bool,
+};
+
+Modal.defaultProps = {
+  closeModal: () => {},
+  visible: true,
 };
 
 export default Modal;
