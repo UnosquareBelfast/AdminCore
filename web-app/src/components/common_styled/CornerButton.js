66adb7c854058d63b3fbf7674bd817daf77dcdde
William-Unosquare <william.park@unosquare.com>
2018-07-27 16:58:41 +0100

    Add CornerButton common_styled component

---
+++ web-app/src/components/common_styled/CornerButton.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export default styled.div`
+  position: absolute;
+  right: 20px;
+  top: 20px;
+
+  button {
+    font-size: 12px;
+    margin-left: 4px;
+    width: auto;
+  }
+`;
