17535bf1dc63013596a3e15648d35b66d7a00c52
William-Unosquare <william.park@unosquare.com>
2018-07-30 15:03:48 +0100

    Remove multi-click from button

--- web-app/src/components/common/Button/index.js
+++ web-app/src/components/common/Button/index.js
@@ -1,44 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { PropTypes as PT } from 'prop-types';
 import { Container } from './styled';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { clickAmount: 0 };
-  }
-
-  // This feature lets you specify an array of labels which will be displayed
-  // to the user before firing the button's action at the final label. Useful
-  // for minimal "are you sure?" messages.
-  multiClick = () => {
-    if (this.state.clickAmount >= this.props.label.length - 1) {
-      this.props.onClick();
-      this.setState({ clickAmount: 0 });
-    } else {
-      this.setState({ clickAmount: this.state.clickAmount + 1 });
-    }
-  };
-
-  render() {
-    const { label } = this.props;
-    const { clickAmount } = this.state;
-    if (typeof label === 'string') {
-      return <Container {...this.props}>{label}</Container>;
-    } else if (Array.isArray(label)) {
-      return (
-        <Container {...this.props} onClick={this.multiClick}>
-          {this.props.label[clickAmount]}
-        </Container>
-      );
-    } else {
-      return 'Specify a label for this button';
-    }
-  }
-}
+const Button = ({ label }) => {
+  return <Container {...this.props}>{label}</Container>;
+};
 
 Button.propTypes = {
-  label: PT.oneOfType([PT.string, PT.array]).isRequired,
+  label: PT.string.isRequired,
   onClick: PT.func.isRequired,
 };
 
