560ab793c66a92e07e4502c6f7ad67ca37a8fec8
William-Unosquare <william.park@unosquare.com>
2018-08-10 15:43:42 +0100

    Remove List components

--- web-app/src/components/ClientList/TableValues.js
+++
@@ -1,9 +0,0 @@
-const clientName = {
-  id: 'clientName',
-  Header: 'Client',
-  accessor: client => client.clientName,
-};
-
-export default {
-  clientName,
-};
--- web-app/src/components/TeamList/TableValues.js
+++
@@ -1,9 +0,0 @@
-const teamName = {
-  id: 'teamName',
-  Header: 'Team Name',
-  accessor: team => team.teamName,
-};
-
-export default {
-  teamName,
-};
