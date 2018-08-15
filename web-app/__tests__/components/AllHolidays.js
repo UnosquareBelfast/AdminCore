c917af16cb3485d90cb8fc83184452611ca48928
Paul Matchett <p.matchett80@gmail.com>
2018-08-15 10:50:26 +0100

    added required prop mock functions to components being tested

--- web-app/__tests__/components/AllHolidays.js
+++ web-app/__tests__/components/AllHolidays.js
@@ -3,21 +3,32 @@ import { shallow } from 'enzyme';
 import { AllHolidays } from '../../src/components/AllHolidays';
 import holidayStatus from '../../src/utilities/holidayStatus';
 
-const dummyHolidays = [{
-  employee: { 
-    forename: 'Joe',
-    surname: 'Bloggs',
+const dummyHolidays = [
+  {
+    employee: {
+      forename: 'Joe',
+      surname: 'Bloggs',
+    },
+    holidayId: 123,
+    date: [1, 1, 2018],
+    dateCreated: new Date(),
+    holidayStatusId: holidayStatus.PENDING,
   },
-  holidayId: 123,
-  date: [1, 1, 2018],
-  dateCreated: new Date(),
-  holidayStatusId: holidayStatus.PENDING,
-}];
+];
+const closeModal = jest.fn();
+const selectHoliday = jest.fn();
 
 describe('AllHolidays', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<AllHolidays holidays={ dummyHolidays }/>);
+    const wrapper = shallow(
+      <AllHolidays
+        holidays={dummyHolidays}
+        selectedHoliday={{}}
+        selectHoliday={selectHoliday}
+        closeModal={closeModal}
+      />,
+    );
     expect(wrapper.exists()).toBe(true);
-    expect(wrapper).toMatchSnapshot();    
+    expect(wrapper).toMatchSnapshot();
   });
 });
