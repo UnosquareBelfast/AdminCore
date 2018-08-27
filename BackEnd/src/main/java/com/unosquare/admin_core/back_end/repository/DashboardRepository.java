9f0f3e2d8185e50626fea75f44b83636f3939d61
Eoin McAfee <eoin.mcafee@unosquare.com>
2018-08-27 12:43:18 +0100

    updating 1st query with new logic to get most relevant events

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/repository/DashboardRepository.java
+++ BackEnd/src/main/java/com/unosquare/admin_core/back_end/repository/DashboardRepository.java
@@ -10,13 +10,25 @@ import java.util.List;
 
 
 public interface DashboardRepository extends JpaRepository<Event, Integer> {
-    @Query(
-            value = "SELECT * FROM Event e WHERE (e.start_Date >= :startDate AND e.end_Date <= :endDate) AND e.employee_id = :employeeId",
+    @Query(value = "SELECT * FROM Event e INNER JOIN Contract c on :employeeId = c.employee_id " +
+            "where " +
+            "(" +
+            "(e.start_date BETWEEN :startDate AND :endDate) OR " +
+            "(e.end_date BETWEEN :startDate AND :endDate) OR " +
+            "(e.start_date > :startDate AND e.end_date < :endDate) " +
+            ") " +
+            "AND " +
+            "(" +
+            "(c.start_date BETWEEN :startDate AND :today) OR " +
+            "(c.end_date BETWEEN :today AND :endDate) OR " +
+            "(c.start_date < :startDate AND (:endDate = NULL OR :endDate > :today))" +
+            ") ",
             nativeQuery = true
     )
     List<Event> findCalendarMonthEventsForEmployee(@Param("employeeId") int employeeId,
                                                    @Param("startDate") LocalDate startDate,
-                                                   @Param("endDate") LocalDate endDate);
+                                                   @Param("endDate") LocalDate endDate,
+                                                   @Param("today") LocalDate today);
 
     @Query(value = "SELECT * FROM Event e INNER JOIN Contract c on e.employee_id = c.employee_id " +
             "where " +
