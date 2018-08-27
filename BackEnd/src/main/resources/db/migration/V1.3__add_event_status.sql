3fd467c1b96076de9da0baeb9b170cf8e4fb4ec7
Paul Brownlee-Unosquare <paul.brownlee@unosquare.com>
2018-08-27 14:53:10 +0100

    Updated schema for event status

--- BackEnd/src/main/resources/db/migration/V1.3__add_event_status.sql
+++ BackEnd/src/main/resources/db/migration/V1.3__add_event_status.sql
@@ -2,7 +2,6 @@ INSERT INTO public.event_status (event_status_id, description)
 VALUES (1, 'Awaiting approval'),
        (2, 'Approved'),
        (3, 'Denied'),
-       (4, 'Mandatory'),
-       (5, 'Taken')
+       (4, 'Cancelled')
 ON CONFLICT (event_status_id)
   DO NOTHING;
