8823aa0d27a2c1dac8665c6759f25b0ce9cb782a
William-Unosquare <william.park@unosquare.com>
2018-08-27 12:21:10 +0100

    Connect FullPageLoader to redux logic

--- web-app/src/components/FullPageLoader/index.jsx
+++ web-app/src/components/FullPageLoader/index.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { PropTypes as PT } from 'prop-types';
+import { connect } from 'react-redux';
+import { isLoading } from '../../reducers';
 import { InnerContainer } from './styled';
 import { Spinner } from '../common';
 
@@ -21,7 +23,13 @@ FullPageLoader.propTypes = {
 };
 
 FullPageLoader.defaultProps = {
-  loading: false,
+  loading: true,
 };
 
-export default FullPageLoader;
+const mapStateToProps = state => {
+  return {
+    loading: isLoading(state),
+  };
+};
+
+export default connect(mapStateToProps)(FullPageLoader);
