d465d0100cc49e74e9448c9c91f65ec59fcd4405
William-Unosquare <william.park@unosquare.com>
2018-08-28 16:42:40 +0100

    Create config for mandatory holidays

---
+++ web-app/src/utilities/mandatoryEvents.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+const currYear = new Date().getFullYear();
+
+// Add mandatory events here. The only fields that need change are title and the dates.
+const mandatoryEvents = [
+  {
+    title: 'Christmas Day',
+    mandatoryDate: `${currYear}-12-25`,
+  },
+  {
+    title: 'New Years Day',
+    mandatoryDate: `${currYear}-12-31`,
+  },
+  {
+    title: 'Memorial Day',
+    mandatoryDate: `${currYear}-05-28`,
+  },
+];
+
+export default mandatoryEvents.map(event => {
+  return {
+    // Format the events
+    title: event.title,
+    start: new moment([event.mandatoryDate], 'YYYY-MM-DD'),
+    end: new moment([event.mandatoryDate], 'YYYY-MM-DD'),
+
+    //Add the event boilerplate
+    eventId: -1,
+    allDay: true,
+    halfDay: false,
+    employee: null,
+    eventStatus: { eventStatusId: 4, description: 'Mandatory' },
+    eventType: { eventTypeId: 1, description: 'Annual leave' },
+  };
+});
