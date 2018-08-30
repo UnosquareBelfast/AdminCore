8e7bf52074e13e3f25635705f59ab1b680db4acd
Kirsty McCrystal <kirstymccrystal@gmail.com>
2018-08-30 13:04:22 +0100

    Improve calendar design.

--- NativeApp/src/components/Home/View/index.js
+++ NativeApp/src/components/Home/View/index.js
@@ -3,8 +3,9 @@ import { PropTypes as PT } from 'prop-types';
 import { View, StyleSheet } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import CustomDay from '../CustomDay';
-import { LIGHTGREY, BLACK } from '../../../styles/colors';
-import { H3_SIZE } from '../../../styles/text';
+import { LIGHTGREY, ACTIVECOLOR, BLACK } from '../../../styles/colors';
+import { H1_SIZE } from '../../../styles/text';
+import { container } from '../../../styles/layout';
 
 const HomeView = (props) => {
   const {
@@ -21,14 +22,19 @@ const HomeView = (props) => {
         markingType="period"
         dayComponent={dayProps => <CustomDay {...dayProps} />}
         theme={{
+          textMonthFontFamily: 'oswaldRegular',
+          textDayFontFamily: 'openSansLight',
+          textDayHeaderFontFamily: 'openSansRegular',
+          textMonthFontSize: H1_SIZE,
+          todayTextColor: ACTIVECOLOR,
+          arrowColor: ACTIVECOLOR,
           monthTextColor: BLACK,
-          textMonthFontSize: H3_SIZE,
-          textMonthFontWeight: 'bold',
           dayTextColor: BLACK,
           textSectionTitleColor: BLACK,
           'stylesheet.calendar.header': {
             week: {
-              marginTop: 7,
+              marginTop: 20,
+              paddingBottom: 13,
               flexDirection: 'row',
               justifyContent: 'space-around',
               borderBottomWidth: 1,
@@ -54,9 +60,7 @@ HomeView.propTypes = {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    backgroundColor: '#fff',
+    ...container,
   },
   calendar: {
     paddingHorizontal: 10,
