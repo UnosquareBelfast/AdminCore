33f05c9fe0d514c28d286f26e5e716f9e87bdfce
Eoin McAfee <eoin.mcafee@unosquare.com>
2018-09-12 15:10:07 +0100

    changed date to string

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/viewModels/dashboard/EventMessageViewModel.java
+++ BackEnd/src/main/java/com/unosquare/admin_core/back_end/viewModels/dashboard/EventMessageViewModel.java
@@ -2,13 +2,11 @@ package com.unosquare.admin_core.back_end.viewModels.dashboard;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-
 @Data
 public class EventMessageViewModel {
     private int eventMessageId;
     private String author;
-    private LocalDate lastModified;
+    private String lastModified;
     private String message;
     private int messageTypeId;
     private String messageTypeDescription;
