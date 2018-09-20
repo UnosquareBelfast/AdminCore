cbde7335a8c7a7e704d05852f700ca2ffd1b124f
William-Unosquare <william.park@unosquare.com>
2018-09-20 15:40:10 +0100

    Add conditonals to allow functionality with two APIS

--- web-app/src/components/UserModal/index.js
+++ web-app/src/components/UserModal/index.js
@@ -6,59 +6,52 @@ import { StyleContainer, Stat, StatWrap } from './styled';
 
 const UserModal = ({
   user,
-  closeModal, 
+  closeModal,
   history,
-  hasPermission,
   approvedDays,
   pendingDays,
+  totalHolidays,
 }) => {
-  
-  if (hasPermission) {
-
-    return (  
-      <Modal closeModal={closeModal}> 
-        <StyleContainer>
-          <div>
-            <h2>
-              {user.forename} {user.surname}
-            </h2>
-            <Email>{user.email}</Email>
-          </div>
-          <StatWrap>
-            <Stat>
-              <h2>{approvedDays + pendingDays} Days</h2>
-              <h4>Holidays Booked</h4>
-            </Stat>
-            <Stat>
-              <h2>{pendingDays} Days</h2>
-              <h4>Holidays Pending</h4>
-            </Stat>
-            <Stat>
-              <h2>{user.totalHolidays - approvedDays - pendingDays} Days</h2>
-              <h4>Holidays Remaining</h4>
-            </Stat>
-          </StatWrap>
-          <Button
-            label="View Full Profile"
-            onClick={() => history.push(`/user/${user.employeeId}`)}
-          />
-        </StyleContainer>
-      </Modal>
-    );
-  }
-  return null;
+  return (
+    <Modal closeModal={closeModal}>
+      <StyleContainer>
+        <div>
+          <h2>{user.name ? user.name : `${user.forename} ${user.surname}`}</h2>
+          <Email>{user.email}</Email>
+        </div>
+        <StatWrap>
+          <Stat>
+            <h2>{approvedDays + pendingDays} Days</h2>
+            <h4>Holidays Booked</h4>
+          </Stat>
+          <Stat>
+            <h2>{pendingDays} Days</h2>
+            <h4>Holidays Pending</h4>
+          </Stat>
+          <Stat>
+            <h2>{totalHolidays - approvedDays - pendingDays} Days</h2>
+            <h4>Holidays Remaining</h4>
+          </Stat>
+        </StatWrap>
+        <Button
+          label="View Full Profile"
+          onClick={() => history.push(`/user/${user.employeeId}`)}
+        />
+      </StyleContainer>
+    </Modal>
+  );
 };
 
 UserModal.propTypes = {
   closeModal: PT.func,
   userDetails: PT.object.isRequired,
   history: PT.object,
-  user: PT.object,   
+  user: PT.object,
   userHolidays: PT.array.isRequired,
-  getHolidays: PT.func.isRequired,
   hasPermission: PT.bool.isRequired,
   approvedDays: PT.number.isRequired,
   pendingDays: PT.number.isRequired,
+  totalHolidays: PT.number,
 };
 
 export default container(UserModal);
