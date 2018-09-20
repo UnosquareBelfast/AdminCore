154f889b1e3c2279bfef207a399b34b2da806463
PaulTummonUnosquare <42933706+PaulTummonUnosquare@users.noreply.github.com>
2018-09-20 15:18:20 +0100

    Delete EmployeeSnapshotViewModel.java

--- BackEnd/src/main/java/com/unosquare/admin_core/back_end/viewModels/dashboard/EmployeeSnapshotViewModel.java
+++
@@ -1,9 +0,0 @@
-package com.unosquare.admin_core.back_end.viewModels.dashboard;
-
-import lombok.Data;
-
-@Data
-public class EmployeeSnapshotViewModel {
-    private String name;
-    private String state;
-}
