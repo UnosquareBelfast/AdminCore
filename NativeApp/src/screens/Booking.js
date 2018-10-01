ebced70bd81386d04b263790af20cf7d89399ddc
lee-cheung <lee.cheung@unosquare.com>
2018-10-01 16:11:02 +0100

    styling header navigation

--- NativeApp/src/screens/Booking.js
+++ NativeApp/src/screens/Booking.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { PropTypes as PT } from 'prop-types';
 import { Icon } from 'react-native-elements';
 import Booking from '../components/Booking';
+import { WHITE } from '../styles/colors';
+import { H2_SIZE } from '../styles/text';
 
 export default class BookingScreen extends Component {
   static navigationOptions = {
     title: 'Booking',
     headerBackTitle: null,
-    headerBackImage: () => <Icon type="font-awesome" name="arrow-left" size={20} color="grey" />,
+    headerTitleStyle: {
+      color: WHITE,
+      marginHorizontal: 0,
+      fontSize: H2_SIZE,
+      fontFamily: 'oswaldRegular',
+    },
+    headerLeft: <Icon containerStyle={{ paddingLeft: 15 }} type="font-awesome" name="arrow-left" size={20} color="white" />,
   }
 
   static propTypes = {
