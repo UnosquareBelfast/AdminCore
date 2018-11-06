bd059f267192b5cfe9b1d7d85ec788d3f1dd67e5
Jamie Higgins <jamie.higgins@unosquare.com>
2018-11-06 14:45:53 +0000

    Added WorkingFromHome Controller and View Model

---
+++ v2/AdminCore.WebApp/Models/WorkingFromHome/WorkingFromHomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using AdminCore.DAL.Models;
+using System;
+
+namespace AdminCore.WebApi.Models.WorkingFromHome
+{
+  public class WorkingFromHomeViewModel
+  {
+    private DateTime dateCreated;
+    private EmployeeDTO employee;
+    private EventDates eventDates;
+    private int eventId;
+    private String eventStatusDescription;
+    private int eventStatusId;
+    private DateTime lastModified;
+  }
+}
