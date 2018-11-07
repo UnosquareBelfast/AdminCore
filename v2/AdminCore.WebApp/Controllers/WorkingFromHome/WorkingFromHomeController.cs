a098c79935ee1e04026a956cdcc606dd38c98e36
Jamie Higgins <jamie.higgins@unosquare.com>
2018-11-07 14:11:35 +0000

    Added Working From Home Stubs

---
+++ v2/AdminCore.WebApp/Controllers/WorkingFromHome/WorkingFromHomeController.cs
@@ -0,0 +1,47 @@
+﻿using AdminCore.WebApi.Models.WorkingFromHome;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AdminCore.WebApi.Controllers
+{
+  [ApiController]
+  [Authorize]
+  [Route("[controller]")]
+  public class WorkingFromHomeController : ControllerBase
+  {
+    private IMapper _mapper;
+    private IEventService _eventService;
+
+    public WorkingFromHomeController(IEventService eventService, IMapper mapper)
+    {
+      _eventService = eventService;
+      _mapper = mapper;
+    }
+
+    [HttpGet]
+    public IList<WorkingFromHomeViewModel> findAll()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpGet("/{workingFromHomeId}")]
+    public WorkingFromHomeViewModel findWorkingFromHomeByID(int eventId)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpGet("/findByEmployeeId/{employeeId}")]
+    public IList<WorkingFromHomeViewModel> findWorkingFromHomeByEmployeeId(int employeeId)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpPost("/")]
+    public void createWorkingFromHome([FromBody] CreateEventViewModel createEventViewModel)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
