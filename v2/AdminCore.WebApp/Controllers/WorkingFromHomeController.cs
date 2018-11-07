86fadca82a6dbb5559c14dc47d991c5dfc826fe1
Jamie Higgins <43340098+Jamie-Higgins@users.noreply.github.com>
2018-11-07 10:48:07 +0000

    Feature/stub out working from home (#279)
    * Added WorkingFromHome Controller and View Model
    
    * Added Create WFH View Model
    
    * Removed unnecessary route and added spacing to view model

---
+++ v2/AdminCore.WebApp/Controllers/WorkingFromHomeController.cs
@@ -0,0 +1,51 @@
+﻿using AdminCore.WebApi.Models.WorkingFromHome;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace AdminCore.WebApi.Controllers
+{
+  [ApiController]
+  [Authorize]
+  [Route("[controller]")]
+  public class WorkingFromHomeController : ControllerBase
+  {
+    private IMapper _mapper;
+    private IEventService _eventService;
+
+    public WorkingFromHomeController(IEventService eventService, IMapper mapper)
+    {
+      _eventService = eventService;
+      _mapper = mapper;
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IList<WorkingFromHomeViewModel> findAll()
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpGet("/{workingFromHomeId}")]
+    [AllowAnonymous]
+    public WorkingFromHomeViewModel findWorkingFromHomeByID(int eventId)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpGet("/findByEmployeeId/{employeeId}")]
+    [AllowAnonymous]
+    public IList<WorkingFromHomeViewModel> findWorkingFromHomeByEmployeeId(int employeeId)
+    {
+      throw new System.NotImplementedException();
+    }
+
+    [HttpPost("/")]
+    [AllowAnonymous]
+    public void createWorkingFromHome([FromBody] CreateEventViewModel createEventViewModel)
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
