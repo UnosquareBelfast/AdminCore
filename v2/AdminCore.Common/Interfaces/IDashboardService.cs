cf21a63505b74fff730d9cab8c44d189181eb8c0
CampbellJGilpin <campbell.gilpin@unosquare.com>
2018-11-09 10:59:19 +0000

    Initial Set Up
    Initial commit - Stubbed out service and controlller methods

---
+++ v2/AdminCore.Common/Interfaces/IDashboardService.cs
@@ -0,0 +1,20 @@
+﻿using AdminCore.DTOs.Dashboard;
+using AdminCore.DTOs.Event;
+using System;
+using System.Collections.Generic;
+
+namespace AdminCore.Common.Interfaces
+{
+  public interface IDashboardService
+  {
+    IList<EventDto> GetEmployeeDashboardEvents(int employeeId, DateTime date);
+
+    IDictionary<string, List<EmployeeSnapshotDto>> GetEmployeeTeamSnapshot(int employeeId);
+
+    IList<EventMessageDto> GetEventMessagesByEventId(int eventid);
+
+    IList<EventDto> GetTeamDashboardEvents(int employeeId, DateTime date);
+
+    IDictionary<string, List<EmployeeSnapshotDto>> GetTeamSnapshotDashboardEvents();
+  }
+}
---
+++ v2/AdminCore.Services/DashboardService.cs
@@ -0,0 +1,36 @@
+﻿using AdminCore.Common.Interfaces;
+using AdminCore.DTOs.Dashboard;
+using AdminCore.DTOs.Event;
+using System;
+using System.Collections.Generic;
+
+namespace AdminCore.Services
+{
+  public class DashboardService : IDashboardService
+  {
+    public IList<EventDto> GetEmployeeDashboardEvents(int employeeId, DateTime date)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IDictionary<string, List<EmployeeSnapshotDto>> GetEmployeeTeamSnapshot(int employeeId)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IList<EventMessageDto> GetEventMessagesByEventId(int eventid)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IList<EventDto> GetTeamDashboardEvents(int employeeId, DateTime date)
+    {
+      throw new NotImplementedException();
+    }
+
+    public IDictionary<string, List<EmployeeSnapshotDto>> GetTeamSnapshotDashboardEvents()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
