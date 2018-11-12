d35489a657dc4660a8c3fc1826c8d59e8e93ca83
CampbellJGilpin <campbell.gilpin@unosquare.com>
2018-11-12 10:21:18 +0000

    Small change to assert

--- v2/AdminCore.WebApi.Tests/ClientControllerTests.cs
+++ v2/AdminCore.WebApi.Tests/ClientControllerTests.cs
@@ -1,60 +1,52 @@
-using System;
 using Xunit;
 
 namespace AdminCore.WebApi.Tests
 {
-  using System.Collections.Generic;
-  using System.Linq;
-
   using AdminCore.Common.Interfaces;
   using AdminCore.DTOs.Client;
   using AdminCore.WebApi.Controllers;
   using AdminCore.WebApi.Models.Client;
-
   using AutoFixture;
-
   using AutoMapper;
-
-  using Microsoft.AspNetCore.Mvc;
-
   using NSubstitute;
+  using System.Collections.Generic;
+  using System.Linq;
 
   public class ClientControllerTests
+  {
+    private readonly IClientService _clientService;
+
+    private readonly ClientController _controller;
+    private readonly IFixture _fixture;
+
+    private readonly IMapper _mapper;
+    public ClientControllerTests()
     {
-      private readonly IClientService _clientService;
+      _clientService = Substitute.For<IClientService>();
+      _mapper = Substitute.For<IMapper>();
+      _fixture = new Fixture();
+      _controller = new ClientController(_clientService, _mapper);
+    }
 
-      private readonly IFixture _fixture;
+    [Fact]
+    public void GetAllClient_WhenCalled_ReturnsAllClients()
+    {
+      // Arrange
+      const int numberOfClients = 9;
 
-      private readonly IMapper _mapper;
+      var clients = _fixture.CreateMany<ClientDto>(numberOfClients).ToList();
+      var clientViewModels = _fixture.CreateMany<ClientViewModel>(numberOfClients).ToList();
 
-      private readonly ClientController _controller;
+      _clientService.GetAll().Returns(clients);
 
-      public ClientControllerTests()
-      {
-        _clientService = Substitute.For<IClientService>();
-        _mapper = Substitute.For<IMapper>();
-        _fixture = new Fixture();
-        _controller = new ClientController(_clientService, _mapper);
-      }
+      _mapper.Map<IList<ClientDto>, List<ClientViewModel>>(Arg.Is(clients)).Returns(clientViewModels);
 
-      [Fact]
-      public void GetAllClient_WhenCalled_ReturnsAllClients()
-      {
-        // Arrange
-        const int numberOfClients = 9;
+      // Act
+      var result = _controller.GetAllClients();
 
-        var clients = _fixture.CreateMany<ClientDto>(numberOfClients).ToList();
-        var clientViewModels = _fixture.CreateMany<ClientViewModel>(numberOfClients).ToList();
-
-        _clientService.GetAll().Returns(clients);
-
-        _mapper.Map<IList<ClientDto>, List<ClientViewModel>>(Arg.Is(clients)).Returns(clientViewModels);
-
-        // Act
-        var result = _controller.GetAllClients();
-        //result.ExecuteResultAsync(new ActionContext());
-        // Assert
-        //Assert.Equal(numberOfClients,  );
-      }
+      // Assert
+      Assert.True(result is List<ClientDto>);
+      Assert.Equal(numberOfClients, (result as List<ClientDto>).Count());
+    }
   }
 }
