0bb9bc9e6134e8ce5ca683e42bc12712a9d907cc
CampbellJGilpin <44165864+CampbellJGilpin@users.noreply.github.com>
2018-11-19 15:45:37 +0000

    Updated ViewModel & StatusViewModel (#290)

--- v2/AdminCore.WebApp/Models/Employee/EmployeeViewModel.cs
+++ v2/AdminCore.WebApp/Models/Employee/EmployeeViewModel.cs
@@ -7,6 +7,8 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+
 namespace AdminCore.WebApi.Models.Employee
 {
   /// <summary>
@@ -14,5 +16,28 @@ namespace AdminCore.WebApi.Models.Employee
   /// </summary>
   public class EmployeeViewModel
   {
+    public string CountryDescription { get; set; }
+
+    public int CountryId { get; set; }
+
+    public string Email { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public string EmployeeRoleDescription { get; set; }
+
+    public int EmployeeRoleId { get; set; }
+
+    public int EmployeeStatusId { get; set; }
+
+    public string Forename { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public string StatusDescription { get; set; }
+
+    public string Surname { get; set; }
+
+    public int TotalHolidays { get; set; }
   }
 }
