cf61c42b3090809fd6290b4cd9284f9c1705fe5d
Jamie Higgins <jamie.higgins@unosquare.com>
2019-01-14 14:34:38 +0000

    Removed unused classes Removed unused methods Removed Event Service from Dashboard Service Made _contractService readonly

--- v2/AdminCore.WebApp/Models/Employee/EmployeeStatusViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AdminCore.WebApi.Models.Employee
-{
-  public class EmployeeStatusViewModel
-  {
-    public string Description { get; set; }
-
-    public int EmployeeStatusId { get; set; }
-  }
-}
